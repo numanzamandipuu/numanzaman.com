39e3172c789828047af2aa89ee22bf9f0d9cefe6
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2026-03-26 02:25:58 +0600

    Add new service pages for Reverse Engineering and Mechanical System Design, create sitemap files, and introduce a starter page template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static'
+  site: 'https://numanzaman.com',
+  output: 'static',
+  integrations: [sitemap()]
 });
